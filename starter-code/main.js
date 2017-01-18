1941ae389c070eab3e08087b93386356a8f7e6c3
Jake Hawkinson <jakehawk1095@gmail.com>
2017-01-18 04:36:09 -0800

    added alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+
+if (cardTwo === cardFour){
+	alert('Sorry, try again.');
+} else if (cardOne === cardFour){
+	alert('You found a match!');
+} else if (cardTwo === cardThree){
+	alert('You found a match!');
+}
