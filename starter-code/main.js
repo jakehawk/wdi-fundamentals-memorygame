ce0b8b55ed35b450052f0653a94f766beb54fa03
Jake Hawkinson <jakehawk1095@gmail.com>
2017-01-23 16:25:31 -0800

    Day one changes.  Added button

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,7 +1,7 @@
 console.log("JS file is connected to HTML! Woo!")
 var cards = ['queen', 'queen', 'king', 'king', 'jack', 'jack'];
 var cardsInPlay = [];
-var boardJS = document.getElementById('game-board');
+var board = document.getElementById('game-board');
 var cardElement = [];
 
 function shuffle(cards) {
@@ -25,18 +25,64 @@ function shuffle(cards) {
 
 shuffle(cards);
 
+function createBoard() {
+	for (var i=0; i<cards.length; i++){
+		var cardElement = document.createElement('div');
+		cardElement.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
+	}
+}
+
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+
+	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = '<img src="images/king.png" alt="King of Spades" />';
+	} else if (this.getAttribute('data-card') === 'queen') {
+		this.innerHTML = '<img src="images/queen.png" alt="Queen of Clubs" />';
+	} else {
+		this.innerHTML = '<img src="images/jack.png" alt="Jack of Hearts" />';
+	}
+
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+function isMatch(cards) {
+	if (cards[0] === cards[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+document.getElementById('reset').onclick = function (cards, cardElement, cardsInPlay) {
+	alert('click click click');
+	for (var i=0; i<cards.length; i++){
+		cardElement[i].innerHTML = '';
+	}
+	cardsInPlay = [];
+}
+
+createBoard();
+/*
 var createCards = function(cardElement, boardJS, cards){
 	for (var i=0; i<cards.length; i++){
 	cardElement[i] = document.createElement('div');
 	cardElement[i].className = 'card';
 	cardElement[i].setAttribute("data-card", cards[i]);
-	console.log()
-	//cardElement[i].innerHTML = '<img src="images/back.png" alt="Card Back" />';
+	console.log();
 	boardJS.appendChild(cardElement[i]);
 	}
 	
 }
-createCards(cardElement, boardJS, cards);
+
 
 var clickLogic = function(cardElement, cardsInPlay){
 	var temp;
@@ -55,41 +101,36 @@ var clickLogic = function(cardElement, cardsInPlay){
 				event.target.innerHTML = '<img src="images/jack.png" alt="Jack of Hearts" />';	
 				console.log('you clicked a jack');
 			}
-			isTwoCards(event.target, cardsInPlay);
+			isTwoCards(event.target, cardsInPlay, cardElement);
 		});
 	}
 	return boardJS;
 }
 
-clickLogic(cardElement, cardsInPlay);
+
 
 function isTwoCards(temp, cardsInPlay, cardElement) {
 	cardsInPlay.push(temp.getAttribute('data-card'));
 
 
 	if (cardsInPlay.length === 2){
-		isMatch(cardsInPlay, cardElement);
+		isMatch(cardsInPlay);
 		cardsInPlay = [];
+		for (var i=0; i<cards.length; i++){
+	 		cardElement[i].innerHTML = '';
+		}
 	}
 }
 
-function isMatch(cardsInPlay, cardElement) {
+function isMatch(cardsInPlay) {
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert('You found a match!');
 	}
 	else{
 		alert('Sorry, try again.');
 	}
-	for (var i=0; i<cards.length; i++){
-	 cardElement[i].innerHTML = '';
-	}
+	cardsInPlay = [];
 }
-/*if (cardTwo === cardFour){
-	alert('Sorry, try again.');
-} else if (cardOne === cardFour){
-	alert('You found a match!');
-} else if (cardTwo === cardThree){
-	alert('You found a match!');
-}
-
+createCards(cardElement, boardJS, cards);
+clickLogic(cardElement, cardsInPlay);
 */
